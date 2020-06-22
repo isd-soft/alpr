4c3579644325ddd3d24ae9c5609c824a1c0e5718
ianic1999 <uiie2639@scs.ubbcluj.ro>
2020-06-22 15:09:17 +0300

    spring boot init

--- alpr-server/.mvn/wrapper/MavenWrapperDownloader.java
+++ alpr-server/.mvn/wrapper/MavenWrapperDownloader.java
@@ -19,8 +19,7 @@ import java.io.*;
 import java.nio.channels.*;
 import java.util.Properties;
 
-public class MavenWrapperDownloader
-{
+public class MavenWrapperDownloader {
 
     private static final String WRAPPER_VERSION = "0.5.6";
     /**
@@ -47,8 +46,7 @@ public class MavenWrapperDownloader
      */
     private static final String PROPERTY_NAME_WRAPPER_URL = "wrapperUrl";
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         System.out.println("- Downloader started");
         File baseDirectory = new File(args[0]);
         System.out.println("- Using base directory: " + baseDirectory.getAbsolutePath());
@@ -57,31 +55,21 @@ public class MavenWrapperDownloader
         // wrapperUrl parameter.
         File mavenWrapperPropertyFile = new File(baseDirectory, MAVEN_WRAPPER_PROPERTIES_PATH);
         String url = DEFAULT_DOWNLOAD_URL;
-        if (mavenWrapperPropertyFile.exists())
-        {
+        if (mavenWrapperPropertyFile.exists()) {
             FileInputStream mavenWrapperPropertyFileInputStream = null;
-            try
-            {
+            try {
                 mavenWrapperPropertyFileInputStream = new FileInputStream(mavenWrapperPropertyFile);
                 Properties mavenWrapperProperties = new Properties();
                 mavenWrapperProperties.load(mavenWrapperPropertyFileInputStream);
                 url = mavenWrapperProperties.getProperty(PROPERTY_NAME_WRAPPER_URL, url);
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 System.out.println("- ERROR loading '" + MAVEN_WRAPPER_PROPERTIES_PATH + "'");
-            }
-            finally
-            {
-                try
-                {
-                    if (mavenWrapperPropertyFileInputStream != null)
-                    {
+            } finally {
+                try {
+                    if (mavenWrapperPropertyFileInputStream != null) {
                         mavenWrapperPropertyFileInputStream.close();
                     }
-                }
-                catch (IOException e)
-                {
+                } catch (IOException e) {
                     // Ignore ...
                 }
             }
@@ -89,40 +77,31 @@ public class MavenWrapperDownloader
         System.out.println("- Downloading from: " + url);
 
         File outputFile = new File(baseDirectory.getAbsolutePath(), MAVEN_WRAPPER_JAR_PATH);
-        if (!outputFile.getParentFile().exists())
-        {
-            if (!outputFile.getParentFile().mkdirs())
-            {
+        if (!outputFile.getParentFile().exists()) {
+            if (!outputFile.getParentFile().mkdirs()) {
                 System.out.println(
                         "- ERROR creating output directory '" + outputFile.getParentFile().getAbsolutePath() + "'");
             }
         }
         System.out.println("- Downloading to: " + outputFile.getAbsolutePath());
-        try
-        {
+        try {
             downloadFileFromURL(url, outputFile);
             System.out.println("Done");
             System.exit(0);
-        }
-        catch (Throwable e)
-        {
+        } catch (Throwable e) {
             System.out.println("- Error downloading");
             e.printStackTrace();
             System.exit(1);
         }
     }
 
-    private static void downloadFileFromURL(String urlString, File destination) throws Exception
-    {
-        if (System.getenv("MVNW_USERNAME") != null && System.getenv("MVNW_PASSWORD") != null)
-        {
+    private static void downloadFileFromURL(String urlString, File destination) throws Exception {
+        if (System.getenv("MVNW_USERNAME") != null && System.getenv("MVNW_PASSWORD") != null) {
             String username = System.getenv("MVNW_USERNAME");
             char[] password = System.getenv("MVNW_PASSWORD").toCharArray();
-            Authenticator.setDefault(new Authenticator()
-            {
+            Authenticator.setDefault(new Authenticator() {
                 @Override
-                protected PasswordAuthentication getPasswordAuthentication()
-                {
+                protected PasswordAuthentication getPasswordAuthentication() {
                     return new PasswordAuthentication(username, password);
                 }
             });
