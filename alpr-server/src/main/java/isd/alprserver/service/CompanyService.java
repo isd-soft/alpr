5bdc07faa2163c351d78b7f6b4485fd6065f182f
SandraRusu17 <sandra.rusu17@gmail.com>
2020-06-25 11:07:04 +0300

    ALPR-2 : Implement CRUD operations for Company Entity

---
+++ alpr-server/src/main/java/isd/alprserver/service/CompanyService.java
@@ -0,0 +1,19 @@
+package isd.alprserver.service;
+
+import isd.alprserver.model.Company;
+
+import java.util.List;
+
+public interface CompanyService {
+    List<Company> getAllCompanies();
+
+    Company addCompany(Company company);
+
+    void deleteCompany(long id);
+
+    List<Company> getCompanies();
+
+    Company getCompanyById(long id);
+
+    Company updateCompany(Company company);
+}
