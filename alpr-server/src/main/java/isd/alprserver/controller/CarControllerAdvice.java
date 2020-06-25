18371b9d465f8f8b0dc0c9f588aec7a4864372e2
cernatinschijana <67259825+cernatinschijana@users.noreply.github.com>
2020-06-25 11:45:49 +0300

    Merge remote-tracking branch 'origin/master' into ALPR-3
    # Conflicts:
    #	alpr-server/src/main/java/isd/alprserver/AlprServerApplication.java
    #	alpr-server/src/main/java/isd/alprserver/controller/CompanyControllerAdvice.java
    #	alpr-server/src/main/java/isd/alprserver/model/User.java
    #	alpr-server/src/main/resources/application.properties

--- alpr-server/src/main/java/isd/alprserver/controller/CarControllerAdvice.java
+++ alpr-server/src/main/java/isd/alprserver/controller/CarControllerAdvice.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackageClasses = {CarController.class})
-public class CompanyControllerAdvice {
+public class CarControllerAdvice {
     @ExceptionHandler (CarNotFoundException.class)
-    public ResponseEntity<String> handleCompanyNotFound(Exception e) {
+    public ResponseEntity<String> handleCarNotFound(Exception e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
