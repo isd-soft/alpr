6ebf8ce62b158f332a45659305c48c1f4779baa2
cernatinschijana <67259825+cernatinschijana@users.noreply.github.com>
2020-06-25 12:53:28 +0300

    Merge remote-tracking branch 'origin/master' into ALPR-3

--- alpr-server/src/main/java/isd/alprserver/AlprServerApplication.java
+++ alpr-server/src/main/java/isd/alprserver/AlprServerApplication.java
@@ -2,17 +2,12 @@ package isd.alprserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AlprServerApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(AlprServerApplication.class, args);
     }
-
 }
