355f1ac279c270d6e79e9980017759b053409283
Dmitrii Hromtov <dmitrii.hromtov@ati.utm.md>
2020-07-02 12:55:37 +0300

    ALPR-17 : Server return also userDTO on authenticate request

--- alpr-server/src/main/java/isd/alprserver/controller/JwtAuthenticationController.java
+++ alpr-server/src/main/java/isd/alprserver/controller/JwtAuthenticationController.java
@@ -1,19 +1,11 @@
 package isd.alprserver.controller;
 
 
-import isd.alprserver.config.JwtTokenUtil;
 import isd.alprserver.model.JwtRequest;
 import isd.alprserver.model.JwtResponse;
 import isd.alprserver.service.AuthenticationService;
-import isd.alprserver.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,26 +20,6 @@ public class JwtAuthenticationController {
 
     @PostMapping(value = "/authenticate")
     public ResponseEntity<JwtResponse> createAuthenticationToken(@RequestBody JwtRequest request) throws Exception {
-        /*
-        authenticate(request.getEmail(), request.getPassword());
-        final UserDetails userDetails = userServiceImpl
-                .loadUserByUsername(request.getEmail());
-        final String token = jwtTokenUtil.generateToken(userDetails);
-        return ResponseEntity.ok(new JwtResponse(token));
-        }
-         */
         return ResponseEntity.ok(this.authenticationService.authenticate(request));
     }
-    /*
-    private void authenticate(String email, String password) throws Exception {
-        try{
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
-        } catch (DisabledException e) {
-            throw new Exception("USER_DISABLED", e);
-        } catch (BadCredentialsException e) {
-            throw new Exception("INVALID_CREDENTIALS", e);
-        }
-    }
-
-     */
 }
--- alpr-server/src/main/java/isd/alprserver/service/AuthenticationServiceImpl.java
+++ alpr-server/src/main/java/isd/alprserver/service/AuthenticationServiceImpl.java
@@ -1,11 +1,11 @@
 package isd.alprserver.service;
 
 import isd.alprserver.config.JwtTokenUtil;
+import isd.alprserver.dto.UserDTO;
 import isd.alprserver.model.JwtRequest;
 import isd.alprserver.model.JwtResponse;
 import isd.alprserver.model.User;
 import isd.alprserver.model.exceptions.InvalidCredentialsException;
-import isd.alprserver.model.exceptions.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -28,21 +28,35 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public JwtResponse authenticate(JwtRequest request) throws InvalidCredentialsException {
         String email = request.getEmail();
-        UserDetails user;
+        UserDetails userDetails;
         try {
-            user = this.userService.loadUserByUsername(email);
-        } catch (UsernameNotFoundException e) {
+            userDetails = this.userService.loadUserByUsername(email);
+        }
+        catch (UsernameNotFoundException e) {
             throw new InvalidCredentialsException("Invalid email or password");
         }
         String rawPassword = request.getPassword();
-        String password = user.getPassword();
-        if(bCryptPasswordEncoder.matches(rawPassword, password)) {
+        String password = userDetails.getPassword();
+        if (bCryptPasswordEncoder.matches(rawPassword, password)) {
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(email, rawPassword)
             );
             SecurityContextHolder.getContext().setAuthentication(authentication);
-            String jwtToken = jwtTokenUtil.generateToken(user);
-            return new JwtResponse(jwtToken);
+            String jwtToken = jwtTokenUtil.generateToken(userDetails);
+
+            User user = userService.getUserByEmail(userDetails.getUsername());
+            UserDTO userDTO = UserDTO.builder()
+                    .role(user.getRole().getName())
+                    .telephoneNumber(user.getTelephoneNumber())
+                    .password(user.getPassword())
+                    .lastName(user.getLastName())
+                    .firstName(user.getFirstName())
+                    .email(user.getEmail())
+                    .company(user.getCompany().getName())
+                    .age(user.getAge())
+                    .build();
+
+            return new JwtResponse(jwtToken, userDTO);
         }
         throw new InvalidCredentialsException("Invalid email or password");
     }
