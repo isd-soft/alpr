1313bf65d10d36906a8979efabf057df7c9d76d5
Dmitrii Hromtov <dmitrii.hromtov@ati.utm.md>
2020-07-03 13:13:17 +0300

    ALPR-19 WEB: implement navigation (still has bugs to fix)

--- alpr-web/src/app/header/header.component.ts
+++ alpr-web/src/app/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from '../auth/authentication.service';
+import {Router} from '@angular/router';
+import {User} from '../shared/user.model';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  user: User;
 
-  ngOnInit(): void {
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.authenticationService.currentUser.subscribe(user => this.user = user);
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
