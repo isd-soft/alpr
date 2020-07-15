63cd0018ae5e105cbc1b7a4df18db56294ff17e2
ianic1999 <uiie2639@scs.ubbcluj.ro>
2020-07-15 10:32:59 +0300

    configure server.js

--- alpr-web/server.js
+++ alpr-web/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/alpr-web'));
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+app.use(forceSSL());
+
+const path = require('path');
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/alpr-web/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-app.listen(process.env.PORT || 3000);
