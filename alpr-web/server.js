0d2e900e950e470312caeb6b1f85495da76ce2f4
ianic1999 <uiie2639@scs.ubbcluj.ro>
2020-07-15 10:26:05 +0300

    configure package jsonm

--- alpr-web/server.js
+++ alpr-web/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/alpr-web'));
 
-const ngApp = express();
-
-ngApp.use(express.static('./dist/angular-forms-validation'));
-
-ngApp.get('/*', function (request, response) {
-  response.sendFile(path.join(__dirname, '/dist/angular-forms-validation/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/alpr-web/index.html'));
 });
 
-ngApp.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
